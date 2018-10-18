a95b26a499be730c0a81dad678f76b700d2f767c
Siddharth Sakhadeo <ssakhadeo@ebay.com>
2018-10-18 16:00:23 -0700

    use regex to get parser and style contents

--- src/util/formatStyles.js
+++ src/util/formatStyles.js
@@ -1,16 +1,10 @@
 const format = require("prettier").format;
+const redent = require("redent");
 
 module.exports = function(code, printContext) {
-    let parser;
-
-    const preprocessor = code.slice(6, 10);
-    if (preprocessor === 'scss') {
-        parser = 'scss';
-    } else if (preprocessor === 'less') {
-        parser = 'less';
-    } else {
-        parser = 'css';
-    }
+    const matches = /^style(?:\.(\S+))?\s*\{\s*([\s\S]*)\s*\}\s*$/.exec(code);
+    const parser = matches[1] || 'css';
+    const block = matches[2];
 
     const config = {
         useTabs: printContext.indentString[0] === '\t',
@@ -18,5 +12,9 @@ module.exports = function(code, printContext) {
         parser
     };
 
-    return format(code, config);
+    code = format(block, config);
+
+    code = `style${parser === "css" ? "" : `.${parser}`} {\n${redent(code, 1, printContext.indentString)}}`;
+
+    return code;
 };
